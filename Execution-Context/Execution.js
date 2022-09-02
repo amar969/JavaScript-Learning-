ba0bb12f5251551f4cbbac25569091a9bc4e9766
Amarjeet Maurya <30472038+amar969@users.noreply.github.com>
2022-09-02 22:51:11 +0530

    Hositing and Scope updated

--- Execution-Context/Execution.js
+++ Execution-Context/Execution.js
@@ -1,17 +1,53 @@
+let b = 20; 
 var a = 10;
 
 function functionA() {
-
+	var c = 20
 	console.log("Start function A");
 
 	function functionB(){
-		console.log("In function B");
+		var d = 30
+		console.log(d);
+		console.log(c);
 	}
 
 	functionB();
 
 }
 
+console.log(a)
+
 functionA();
 
 console.log("GlobalContext");
+
+
+let outputElem = document.getElementById("output");
+
+let userLanguages = {
+  "Mike": "en",
+  "Teresa": "es"
+};
+
+function greetUser(user) {
+  function localGreeting(user) {
+    let greeting;
+    let language = userLanguages[user];
+
+    switch(language) {
+      case "es":
+        greeting = `¡Hola, ${user}!`;
+        break;
+      case "en":
+      default:
+        greeting = `Hello, ${user}!`;
+        break;
+    }
+    return greeting;
+  }
+  outputElem.innerHTML += `${localGreeting(user)}<br>\r`;
+}
+
+greetUser("Mike");
+greetUser("Teresa");
+greetUser("Veronica");
